6bb1bc8da6536c5bf9931749acd0fe96df1c624f
Zaiden <65905477+Synceratus@users.noreply.github.com>
2022-04-28 18:47:14 +0200

    Added more assets.

---
+++ game/assets/maps/ObjectsTileset.tsx
@@ -0,0 +1,238 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="ObjectsTileset" tilewidth="175" tileheight="190" tilecount="78" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="45" height="22" source="Images/Benches/1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="48" height="22" source="Images/Benches/2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="45" height="10" source="Images/Benches/3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="48" height="10" source="Images/Benches/4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="33" source="Images/Bushes/1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="33" source="Images/Bushes/2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="33" source="Images/Bushes/3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="33" source="Images/Bushes/4.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="25" source="Images/Bushes/5.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="25" source="Images/Bushes/6.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="25" source="Images/Bushes/7.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="25" source="Images/Bushes/8.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="16" source="Images/Bushes/9.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="16" source="Images/Bushes/10.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="16" source="Images/Bushes/11.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="16" source="Images/Bushes/12.png"/>
+ </tile>
+ <tile id="16">
+  <image width="40" height="17" source="Images/Bushes/13.png"/>
+ </tile>
+ <tile id="17">
+  <image width="40" height="16" source="Images/Bushes/14.png"/>
+ </tile>
+ <tile id="18">
+  <image width="38" height="13" source="Images/Bushes/15.png"/>
+ </tile>
+ <tile id="19">
+  <image width="22" height="10" source="Images/Bushes/16.png"/>
+ </tile>
+ <tile id="20">
+  <image width="50" height="25" source="Images/Bushes/17.png"/>
+ </tile>
+ <tile id="21">
+  <image width="41" height="24" source="Images/Bushes/18.png"/>
+ </tile>
+ <tile id="22">
+  <image width="43" height="22" source="Images/Bushes/19.png"/>
+ </tile>
+ <tile id="23">
+  <image width="30" height="19" source="Images/Bushes/20.png"/>
+ </tile>
+ <tile id="24">
+  <image width="35" height="64" source="Images/Fence/1.png"/>
+ </tile>
+ <tile id="25">
+  <image width="35" height="64" source="Images/Fence/2.png"/>
+ </tile>
+ <tile id="26">
+  <image width="17" height="41" source="Images/Fence/2_1.png"/>
+ </tile>
+ <tile id="27">
+  <image width="35" height="64" source="Images/Fence/3.png"/>
+ </tile>
+ <tile id="28">
+  <image width="35" height="64" source="Images/Fence/4.png"/>
+ </tile>
+ <tile id="29">
+  <image width="3" height="41" source="Images/Fence/4_1.png"/>
+ </tile>
+ <tile id="30">
+  <image width="35" height="64" source="Images/Fence/5.png"/>
+ </tile>
+ <tile id="31">
+  <image width="48" height="64" source="Images/Fence/6.png"/>
+ </tile>
+ <tile id="32">
+  <image width="48" height="64" source="Images/Fence/7.png"/>
+ </tile>
+ <tile id="33">
+  <image width="72" height="72" source="Images/Fountain/1.png"/>
+ </tile>
+ <tile id="34">
+  <image width="72" height="72" source="Images/Fountain/2.png"/>
+ </tile>
+ <tile id="35">
+  <image width="8" height="6" source="Images/Grass/1.png"/>
+ </tile>
+ <tile id="36">
+  <image width="8" height="6" source="Images/Grass/2.png"/>
+ </tile>
+ <tile id="37">
+  <image width="8" height="7" source="Images/Grass/3.png"/>
+ </tile>
+ <tile id="38">
+  <image width="6" height="5" source="Images/Grass/4.png"/>
+ </tile>
+ <tile id="39">
+  <image width="8" height="8" source="Images/Grass/5.png"/>
+ </tile>
+ <tile id="40">
+  <image width="7" height="5" source="Images/Grass/6.png"/>
+ </tile>
+ <tile id="41">
+  <image width="7" height="7" source="Images/Grass/7.png"/>
+ </tile>
+ <tile id="42">
+  <image width="7" height="5" source="Images/Grass/8.png"/>
+ </tile>
+ <tile id="43">
+  <image width="6" height="5" source="Images/Grass/9.png"/>
+ </tile>
+ <tile id="44">
+  <image width="10" height="7" source="Images/Grass/10.png"/>
+ </tile>
+ <tile id="45">
+  <image width="7" height="5" source="Images/Grass/11.png"/>
+ </tile>
+ <tile id="46">
+  <image width="7" height="5" source="Images/Grass/12.png"/>
+ </tile>
+ <tile id="47">
+  <image width="5" height="4" source="Images/Grass/13.png"/>
+ </tile>
+ <tile id="48">
+  <image width="8" height="6" source="Images/Grass/14.png"/>
+ </tile>
+ <tile id="49">
+  <image width="7" height="6" source="Images/Grass/15.png"/>
+ </tile>
+ <tile id="50">
+  <image width="8" height="8" source="Images/Leaf/1.png"/>
+ </tile>
+ <tile id="51">
+  <image width="8" height="8" source="Images/Leaf/2.png"/>
+ </tile>
+ <tile id="52">
+  <image width="8" height="8" source="Images/Leaf/3.png"/>
+ </tile>
+ <tile id="53">
+  <image width="8" height="8" source="Images/Leaf/4.png"/>
+ </tile>
+ <tile id="54">
+  <image width="8" height="8" source="Images/Leaf/5.png"/>
+ </tile>
+ <tile id="55">
+  <image width="8" height="8" source="Images/Leaf/6.png"/>
+ </tile>
+ <tile id="56">
+  <image width="32" height="25" source="Images/Other/Box.png"/>
+ </tile>
+ <tile id="57">
+  <image width="15" height="13" source="Images/Other/Garbage_Can1.png"/>
+ </tile>
+ <tile id="58">
+  <image width="15" height="13" source="Images/Other/Garbage_Can2.png"/>
+ </tile>
+ <tile id="59">
+  <image width="23" height="32" source="Images/Other/Ladder1.png"/>
+ </tile>
+ <tile id="60">
+  <image width="23" height="32" source="Images/Other/Ladder2.png"/>
+ </tile>
+ <tile id="61">
+  <image width="92" height="48" source="Images/Other/Ramp1.png"/>
+ </tile>
+ <tile id="62">
+  <image width="92" height="48" source="Images/Other/Ramp2.png"/>
+ </tile>
+ <tile id="63">
+  <image width="144" height="39" source="Images/Other/Rapm3.png"/>
+ </tile>
+ <tile id="64">
+  <image width="26" height="7" source="Images/Other/Skateboard1.png"/>
+ </tile>
+ <tile id="65">
+  <image width="11" height="20" source="Images/Other/Skateboard2.png"/>
+ </tile>
+ <tile id="66">
+  <image width="10" height="20" source="Images/Other/Skateboard3.png"/>
+ </tile>
+ <tile id="67">
+  <image width="12" height="20" source="Images/Other/Skateboard4.png"/>
+ </tile>
+ <tile id="68">
+  <image width="62" height="103" source="Images/Other/Tree1.png"/>
+ </tile>
+ <tile id="69">
+  <image width="124" height="129" source="Images/Other/Tree2.png"/>
+ </tile>
+ <tile id="70">
+  <image width="175" height="190" source="Images/Other/Tree3.png"/>
+ </tile>
+ <tile id="71">
+  <image width="175" height="190" source="Images/Other/Tree4.png"/>
+ </tile>
+ <tile id="72">
+  <image width="10" height="7" source="Images/Stones/1.png"/>
+ </tile>
+ <tile id="73">
+  <image width="22" height="14" source="Images/Stones/2.png"/>
+ </tile>
+ <tile id="74">
+  <image width="27" height="16" source="Images/Stones/3.png"/>
+ </tile>
+ <tile id="75">
+  <image width="39" height="17" source="Images/Stones/4.png"/>
+ </tile>
+ <tile id="76">
+  <image width="46" height="22" source="Images/Stones/5.png"/>
+ </tile>
+ <tile id="77">
+  <image width="70" height="44" source="Images/Stones/6.png"/>
+ </tile>
+</tileset>
