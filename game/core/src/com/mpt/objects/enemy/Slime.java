da712184fe6d7d2899687df9fda959db4963f859
DEV_niz <73193016+VisualNizar@users.noreply.github.com>
2022-05-10 10:52:04 +0200

    Update Slime.java

--- game/core/src/com/mpt/objects/enemy/Slime.java
+++ game/core/src/com/mpt/objects/enemy/Slime.java
@@ -4,7 +4,27 @@ import com.badlogic.gdx.physics.box2d.Body;
 
 public class Slime extends Enemy {
 
+    final static int maxPosX = -50;
+
+    final static int maxPosY = 50; //same qui
+
+    final static int rangeOfAction = 1; //per ora non fa nulla
     public Slime(float width, float height, Body body) {
         super(width, height, body);
+
+    }
+
+    public void movementSlime(){
+
+    }
+
+    public void getX() {
+        float x;
+        x = body.getPosition().x;
+    }
+
+    public void getY() {
+        float y;
+        y = body.getPosition().y;
     }
 }
