38f4c931ed7e6cf10a7eab6b0571ff4e7bf0ec8d
Zaiden <65905477+Synceratus@users.noreply.github.com>
2022-05-16 12:33:39 +0200

    Create CombatHandler.java

---
+++ game/core/src/com/mpt/handlers/CombatHandler.java
@@ -0,0 +1,30 @@
+package com.mpt.handlers;
+
+import com.mpt.objects.GameEntity;
+import com.mpt.objects.enemy.Enemy;
+import com.mpt.objects.player.Player;
+
+public class CombatHandler {
+    public CombatHandler() {};
+
+    public static void attackPlayer(GameEntity attacker, GameEntity victim) {
+        Player player;
+        Enemy enemy;
+        
+        if(attacker instanceof Player && victim instanceof Enemy) {
+            player = (Player) attacker;
+            enemy = (Enemy) victim;
+
+            // Codice player attacca nemico
+
+        }
+        else if(attacker instanceof Enemy && victim instanceof Player) {
+            enemy = (Enemy) attacker;
+            player = (Player) victim;
+
+            // Codice nemico attacca player
+
+        }
+    }
+
+}
