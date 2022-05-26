3efa7ea815ac1a8b53c0f614704004e99c3c5192
DEV_niz <73193016+VisualNizar@users.noreply.github.com>
2022-05-26 08:51:20 +0200

    changes enemy
    enemy attack

--- game/core/src/com/mpt/objects/enemy/Enemy.java
+++ game/core/src/com/mpt/objects/enemy/Enemy.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.mpt.handlers.AnimationHandler;
+import com.mpt.handlers.CombatHandler;
 import com.mpt.handlers.MapHandler;
 import com.mpt.objects.player.Player;
 import com.mpt.objects.GameEntity;
@@ -18,19 +19,15 @@ import javax.swing.*;
 import static com.mpt.constants.Constants.PPM;
 
 public abstract class Enemy extends GameEntity{
-    private float actuallyPosX = body.getPosition().x;
-    private float actuallyPosY = body.getPosition().y;
-    private float playerMoveToX;
-    private float playerMoveToY;
-    private double diffY;
-    private double diffX;
-    private double angle;
+
+    private boolean distanceToAttackPlayer;
     private float initialPosX; //initial xPos of slime
     private float initialPosY; //initial yPos of slime
-    private int killCounter = 0; //player's frags
-    final private float distance = 2f;
+    private int killCounter; //player's frags
     private int damageToPlayer;
     private GameScreen gameScreen;
+
+    private CombatHandler combatHandler;
     private boolean playerHasBeenSpotted;
     private boolean playerHasBeenDefeat;
     private float xMaxLimitDX; //limit position on right side
@@ -38,7 +35,6 @@ public abstract class Enemy extends GameEntity{
     private boolean switchDirectionToRight = false; //flag that says if enemy switched direction
     private boolean switchDirectionToLeft = false;
     protected float walkSpeed;
-    private float x,y;
     // Enemy States
     public enum EnemyState {
         IDLE,
@@ -57,23 +53,19 @@ public abstract class Enemy extends GameEntity{
         initialPosY = body.getPosition().y; //initial position of enemy
         playerHasBeenSpotted = false;
         playerHasBeenDefeat = false;
+        distanceToAttackPlayer = false;
         this.gameScreen = gameScreen;
         direction = "RIGHT";
         animationHandler = new AnimationHandler();
+        enemyState = EnemyState.IDLE;
     }
-
     @Override
     public void update(float delta) {
-        if(!playerSpotted(gameScreen.getPlayer())) {
+        if (!playerSpotted(gameScreen.getPlayer()))
             enemyMovements();
-            //System.out.println("players has NOT been spotted");
-        }else {
+        else
             lurkTarget(gameScreen.getPlayer());
-            //System.out.println("player has BEEN spotted");
-            //System.out.println("player's health"+gameScreen.getPlayer().getHealth());
-        }
     }
-
     @Override
     public void render(SpriteBatch batch) {
         TextureRegion currentFrame = animationHandler.getFrame();
@@ -86,6 +78,25 @@ public abstract class Enemy extends GameEntity{
         batch.draw(currentFrame, tX, tY);
     }
 
+
+    private void checkPlayerDeath() {
+        if(enemyState.equals(EnemyState.DYING)) {
+            //body.setTransform(respawnPosition.x, respawnPosition.y, body.getAngle());
+            enemyState = EnemyState.IDLE;
+        }
+    }
+
+    private void respawnOnVoidPosition() {
+        if(body.getPosition().y < 0)
+            enemyState= EnemyState.DYING;
+    }
+
+    public void enemyAttackPlayer(Player player) {
+        if (enemyReadyToAttack(player)) {
+            combatHandler.attack(this, player);
+            System.out.println("Enemy is attacking PLAYER!");
+        }
+    }
     public void enemyMovements(){
         enemyState = EnemyState.WALKING;
         xMaxLimitDX = initialPosX + 2.4f;
@@ -108,27 +119,31 @@ public abstract class Enemy extends GameEntity{
             switchDirectionToRight = true;
         }
     }
-
     public boolean playerSpotted(Player player) {
-        //System.out.println(Math.abs(player.getBody().getPosition().x - body.getPosition().x));
         playerHasBeenSpotted = Math.abs(player.getBody().getPosition().x - body.getPosition().x) < 8f;
         return playerHasBeenSpotted;
     }
 
+    public boolean enemyReadyToAttack(Player player){
+        distanceToAttackPlayer = Math.abs(player.getBody().getPosition().x - body.getPosition().x) < 0.5f;
+        return distanceToAttackPlayer;
+    }
     public void lurkTarget(Player player){
-        if(playerSpotted(player) && Math.abs((int)player.getBody().getPosition().x - (int)body.getPosition().x) != 0) {
-            if (player.getBody().getPosition().x < this.getBody().getPosition().x) {
-                body.setLinearVelocity(walkSpeed * (-3f), body.getLinearVelocity().y);
-                setFacingRight();
-            } else if(player.getBody().getPosition().x > this.getBody().getPosition().x) {
-                body.setLinearVelocity(walkSpeed * (3f), body.getLinearVelocity().y);
-                setFacingLeft();
+        if(!enemyReadyToAttack(player)) {
+            if (playerSpotted(player) && Math.abs((int) player.getBody().getPosition().x - (int) body.getPosition().x) != 0) {
+                if (player.getBody().getPosition().x < this.getBody().getPosition().x) {
+                    body.setLinearVelocity(walkSpeed * (-3f), body.getLinearVelocity().y);
+                    setFacingRight();
+                } else if (player.getBody().getPosition().x > this.getBody().getPosition().x) {
+                    body.setLinearVelocity(walkSpeed * (3f), body.getLinearVelocity().y);
+                    setFacingLeft();
+                }
+            } else if (Math.abs((int) player.getBody().getPosition().x - (int) body.getPosition().x) == 0) {
+                this.getBody().setLinearVelocity(0, this.getBody().getLinearVelocity().y);
+                enemyState = EnemyState.IDLE;
             }
         }
-        else if(Math.abs((int)player.getBody().getPosition().x - (int)body.getPosition().x) == 0) {
-            this.getBody().setLinearVelocity(0, this.getBody().getLinearVelocity().y);
-            enemyState = EnemyState.IDLE;
-        }
+       enemyAttackPlayer(player);
     }
 
     protected void loadSprites() {
@@ -137,16 +152,7 @@ public abstract class Enemy extends GameEntity{
         charset = new TextureAtlas(Gdx.files.internal("./enemies/"+enemyName+"/attack.atlas"));
         float FRAME_TIME = 1 / 6f;
         this.animationHandler.add("attack", new Animation<>(FRAME_TIME, charset.findRegions("attack")));
-/*
-        charset = new TextureAtlas(Gdx.files.internal("./enemies/"+enemyName+"/attack2.atlas"));
-        this.animationHandler.add("attack2", new Animation<>(FRAME_TIME, charset.findRegions("attack2")));
 
-        charset = new TextureAtlas(Gdx.files.internal("./enemies/"+enemyName+"/attack3.atlas"));
-        this.animationHandler.add("attack3", new Animation<>(FRAME_TIME, charset.findRegions("attack3")));
-
-        charset = new TextureAtlas(Gdx.files.internal("./enemies/"+enemyName+"/attack4.atlas"));
-        this.animationHandler.add("attack4", new Animation<>(FRAME_TIME, charset.findRegions("attack4")));
-*/
         charset = new TextureAtlas(Gdx.files.internal("./enemies/"+enemyName+"/death.atlas"));
         this.animationHandler.add("death", new Animation<>(FRAME_TIME, charset.findRegions("death")));
 
@@ -155,10 +161,7 @@ public abstract class Enemy extends GameEntity{
 
         charset = new TextureAtlas(Gdx.files.internal("./enemies/"+enemyName+"/idle.atlas"));
         this.animationHandler.add("idle", new Animation<>(FRAME_TIME, charset.findRegions("idle")));
-/*
-        charset = new TextureAtlas(Gdx.files.internal("./enemies/"+enemyName+"/sneer.atlas"));
-        this.animationHandler.add("sneer", new Animation<>(FRAME_TIME, charset.findRegions("sneer")));
-*/
+
         charset = new TextureAtlas(Gdx.files.internal("./enemies/"+enemyName+"/walk.atlas"));
         this.animationHandler.add("walk", new Animation<>(FRAME_TIME, charset.findRegions("walk")));
 
