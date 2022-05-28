ccba3e5ffd5202bcfd4cf1d6bb2199cb5d38950e
Zaiden <65905477+Synceratus@users.noreply.github.com>
2022-05-28 20:36:23 +0200

    Added menu background sound

--- game/core/src/com/mpt/platform/PlatformGame.java
+++ game/core/src/com/mpt/platform/PlatformGame.java
@@ -1,6 +1,7 @@
 package com.mpt.platform;
 
 import com.badlogic.gdx.Game;
+import com.mpt.modules.MusicModule;
 
 public class PlatformGame extends Game {
 
@@ -8,13 +9,17 @@ public class PlatformGame extends Game {
 
 	@Override
 	public void create() {
+		MusicModule.setup();
 		menuScreen = new MenuScreen();
+		MusicModule.getMainMenuMusic().play();
+		MusicModule.getMainMenuMusic().setLooping(true);
 		setScreen(menuScreen);
 	}
 
 	@Override
 	public void dispose() {
 		menuScreen.dispose();
+		MusicModule.dispose();
 	}
 
 	@Override
