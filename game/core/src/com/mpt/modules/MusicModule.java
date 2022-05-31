65005377861fb07caa54d7c9afc965147884108c
DEV_niz <73193016+VisualNizar@users.noreply.github.com>
2022-05-31 17:18:04 +0200

    changes

--- game/core/src/com/mpt/modules/MusicModule.java
+++ game/core/src/com/mpt/modules/MusicModule.java
@@ -5,27 +5,31 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 
 public class MusicModule {
+    //Musics
     private static Music mainMenuMusic;
+    private static Music gameOverSound;
+    //Sounds
+    private static Sound footStepsSound;
     private static Sound rolloverSound;
     private static Sound collectCoinSound;
-    private static Music footStepsSound;
     private static Sound checkPointSound;
-    private static Music gameOverSound;
-
     private static Sound jumpSound;
     private static Sound doubleJumpSound;
     private static Sound portalSound;
+
+    private static Sound playerDeathSound;
 
     public static void setup() {
         mainMenuMusic = Gdx.audio.newMusic(Gdx.files.internal("audio/Poseidon's Realm.wav"));
         rolloverSound = Gdx.audio.newSound(Gdx.files.internal("audio/ui_sfx/OptionRollover.wav"));
         collectCoinSound = Gdx.audio.newSound(Gdx.files.internal("audio/game_sfx/CollectCoin.mp3"));
-        footStepsSound = Gdx.audio.newMusic(Gdx.files.internal("audio/game_sfx/FootstepSound.ogg"));
+        footStepsSound = Gdx.audio.newSound(Gdx.files.internal("audio/game_sfx/FootstepSound.ogg"));
         checkPointSound = Gdx.audio.newSound(Gdx.files.internal("audio/game_sfx/Checkpoint.ogg"));
         gameOverSound = Gdx.audio.newMusic(Gdx.files.internal("audio/game_sfx/GameoverSound.wav"));
         jumpSound = Gdx.audio.newSound(Gdx.files.internal("audio/game_sfx/jumpSound.mp3"));
         doubleJumpSound = Gdx.audio.newSound(Gdx.files.internal("audio/game_sfx/doubleJumpSound.mp3"));
         portalSound = Gdx.audio.newSound(Gdx.files.internal("audio/game_sfx/portalSound.mp3"));
+        playerDeathSound = Gdx.audio.newSound(Gdx.files.internal("audio/game_sfx/humanDeath.mp3"));
     }
 
     public static Music getGameOverSound() {return gameOverSound;}
@@ -33,7 +37,7 @@ public class MusicModule {
         return mainMenuMusic;
     }
 
-    public static Music getFootStepsMusic() {
+    public static Sound getFootStepsMusic() {
         return footStepsSound;
     }
     public static Sound getRolloverSound() {
@@ -56,7 +60,11 @@ public class MusicModule {
     public static Sound getPortalSound() {
         return portalSound;
     }
+    public static Sound getPlayerDeathSound() {
+        return playerDeathSound;
+    }
     public static void dispose(){
+        playerDeathSound.dispose();
         portalSound.dispose();
         jumpSound.dispose();
         doubleJumpSound.dispose();
